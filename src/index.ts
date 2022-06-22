c4ee86005cf2714ddc602225b1d6c2b81e265765
Pawel  Kurek <pawel.kurek975@gmail.com>
2022-06-22 20:24:51 +0200

    added local storage saving

--- src/index.ts
+++ src/index.ts
@@ -4,27 +4,94 @@ const addTaskInput: HTMLInputElement = document.querySelector("#add-task__input"
 const addTaskSubmit: HTMLInputElement = document.querySelector(".add-task__submit")!;
 const tasksContainer: HTMLDivElement = document.querySelector(".tasks-container__tasks")!;
 
-// Create task and Add it to task container
-function addTask(taskName: string): void {
+interface TaskListElement {
+    id: string | number,
+    taskName: string
+}
 
-    const singleTaskContainer = document.createElement("div");
-    singleTaskContainer.classList.add('singleTask')
-    singleTaskContainer.setAttribute("id", <string>generateId())
+let taskList: TaskListElement[] = getTasksFromLocalStorage()
 
-    const taskNameContainer = document.createElement("span");
-    taskNameContainer.textContent = taskName;
-    const taskDeleteBtn = document.createElement("button");
+// Initial tasks render
+renderTasks(taskList)
+
+// Create task object and add it to taskList array
+function addToTaskList(taskName: string): void {
+
+    const newTask: TaskListElement = 
+    {
+        id: generateId(),
+        taskName: taskName
+    }
+    taskList.push(newTask);
+
+    resetAddTaskInput();
+}
+
+
+function renderTasks(taskList: TaskListElement[]) {
+
+    // clear tasks container before re-render
+    [...tasksContainer.children].forEach(task => {
+        tasksContainer.removeChild(task);
+    });
+
+    // re-render tasks
+    taskList.forEach(task => {
+        const singleTaskContainer = document.createElement("div");
+        singleTaskContainer.classList.add('singleTask')
+        singleTaskContainer.setAttribute("id", <string>task.id)
+
+        const taskNameContainer = document.createElement("span");
+        taskNameContainer.textContent = task.taskName;
+        const taskDeleteBtn = document.createElement("button");
+        
+        singleTaskContainer.appendChild(taskNameContainer);
+        singleTaskContainer.appendChild(taskDeleteBtn);
+
+        tasksContainer.appendChild(singleTaskContainer);
+    })
+
+    // Set every task delete btn to handle delete process
+    const allTasks = [...tasksContainer.children];
+    allTasks.forEach(task => {
+        task.children[1].addEventListener("click", handleDeleteTaskBtnClick)
+    });
+
+    saveTasksToLocalStorage(taskList);
     
-    singleTaskContainer.appendChild(taskNameContainer);
-    singleTaskContainer.appendChild(taskDeleteBtn);
-
-    tasksContainer.appendChild(singleTaskContainer);
 }
 
 // Delete task based on id
-function deleteTask(taskId: number | string): void {
-    const taskToDelete = [...tasksContainer.children].filter(task => task.id === taskId)[0]
-    if (taskToDelete) tasksContainer.removeChild(taskToDelete);
+function deleteTask(clickedTaskId: number | string): void {
+
+    taskList.forEach(task => {
+        if (task.id === Number(clickedTaskId)) {
+            taskList.splice(taskList.indexOf(task), 1)
+        }
+    })
+}
+
+function resetAddTaskInput():void {
+    addTaskInput.value = "";
+}
+
+function saveTasksToLocalStorage(taskList: TaskListElement[]): void {
+    localStorage.setItem("tasks", JSON.stringify(taskList))
+}
+
+function getTasksFromLocalStorage(): TaskListElement[] {
+    
+    const localStorageRecord = localStorage.getItem("tasks");
+
+    if (localStorageRecord) {
+        
+        const localStorageTasks = JSON.parse(localStorageRecord);
+        return localStorageTasks 
+        
+    } else {
+        return [];
+    }
+
 }
 
 
@@ -32,13 +99,20 @@ function deleteTask(taskId: number | string): void {
 
 function handleDeleteTaskBtnClick(e: any): void {
     deleteTask(e.target.parentNode.id);
+    renderTasks(taskList)
 }
 
 function handleAddTaskFormSubmit(e: any): void {
     e.preventDefault();
-    addTask(addTaskInput.value);
 
-
+    if (addTaskInput.value.length >= 3) {
+        addToTaskList(addTaskInput.value);
+    } else {
+        alert("Task must contain at least 3 letters");
+        addTaskInput.focus();
+    }
+    
+    renderTasks(taskList)
 }
 
 
@@ -55,18 +129,3 @@ function generateId(): TaskId {
     const id: TaskId = Math.floor(Math.random() * 100000);
     return id
 }
-
-
-/* Observers */
-
-const taskObserver = new MutationObserver(() => {
-    // Set every task delete btn to handle delete process
-    const allTasks = [...tasksContainer.children];
-    allTasks.forEach(task => {
-        task.children[1].addEventListener("click", handleDeleteTaskBtnClick)
-    });
-});
-
-// If new task is created, event listener is added to task delete btn
-taskObserver.observe(tasksContainer, {childList: true});
-
